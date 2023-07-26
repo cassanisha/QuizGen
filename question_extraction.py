5289f4c714a9737d0913d7703f7efc5a4aad850a
Anisha <112388219+cassanisha@users.noreply.github.com>
2023-07-26 09:46:08 +0530

    Importing necessary libraries question_extraction.py

---
+++ question_extraction.py
@@ -0,0 +1,8 @@
+'''This file contains the module for generating
+'''
+import nltk
+import spacy
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+
