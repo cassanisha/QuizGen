1d9cd20b560c4d7af3625566f29a5ccdf2e76d6a
Anisha <112388219+cassanisha@users.noreply.github.com>
2023-07-26 09:58:10 +0530

    Update incorrect_answer_generation.py

--- incorrect_answer_generation.py
+++ incorrect_answer_generation.py
@@ -24,4 +24,23 @@ class IncorrectAnswerGenerator:
             self.all_words.extend(word_tokenize(sent))
         self.all_words = list(set(self.all_words))
 
-        
+    def get_all_options_dict(self, answer, num_options):
+        ''' This method returns a dict
+        of 'num_options' options out of
+        which one is correct and is the answer
+        '''
+        options_dict = dict()
+        try:
+            similar_words = self.model.similar_by_word(answer, topn=15)[::-1]
+
+            for i in range(1, num_options + 1):
+                options_dict[i] = similar_words[i - 1][0]
+
+
+
+
+        replacement_idx = random.randint(1, num_options)
+
+        options_dict[replacement_idx] = answer
+
+        return options_dict
